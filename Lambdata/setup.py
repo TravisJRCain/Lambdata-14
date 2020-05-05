1e391deda5858fef360884d9eb6ec032fc4aca2e
Travis Cain <Travis@Traviss-MacBook-Air.local>
2020-05-05 16:05:50 -0700

    Setup repo

--- Lambdata/setup.py
+++ Lambdata/setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(
-    name="Lambdata-14", # the name that you will install via pip
+    name="lambdata-Travis-Cain", # the name that you will install via pip
     version="1.0",
     author="Travis Cain",
     author_email="TravisJRCain@Outlook.Com",
@@ -14,7 +14,7 @@ setup(
     long_description=long_description,
     long_description_content_type="text/markdown", # required if using a md file for long desc
     #license="MIT",
-    url="https://github.com/TravisJRCain/Lambdata-14",
+    url="https://github.com/TravisJRCain/lambdata-Travis-Cain",
     #keywords="",
     packages=find_packages() # ["Lambdata"]
 )
