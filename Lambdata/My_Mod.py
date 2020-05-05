1d3f9a19ff2db82bc01bb529e4e14877bb85cc5c
Travis Cain <Travis@Traviss-MacBook-Air.local>
2020-05-04 18:08:28 -0700

    Setup Local Imports

--- Lambdata/My_Mod.py
+++ Lambdata/My_Mod.py
@@ -2,5 +2,11 @@
 def enlarge(n):
     return n*100
 
+
+
 x = 5
 print(enlarge(x))
+
+
+z = input('Please choose a number to enlarge:')
+print(enlarge(z))
--- Lambdata/My_Script.py
+++ Lambdata/My_Script.py
@@ -1,8 +1,14 @@
 import pandas
 
+# import My_Mod
+from My_Mod import enlarge
+
 print('Hello World')
 
 print(2+2)
 
 df = pandas.DataFrame({'a':[1,2,3], 'b':[4,5,6]})
 print(df)
+
+y = 9
+print(enlarge(y))
