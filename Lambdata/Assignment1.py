3ffd100a73a1b9bd12647679d58765bfc2d6c214
Travis Cain <Travis@Traviss-MacBook-Air.local>
2020-05-06 11:32:43 -0700

    New Assignment 1

--- Lambdata/Assignment1.py
+++ Lambdata/Assignment1.py
@@ -1,10 +1,37 @@
-# Lambdata/Assignment1.py
-import pandas as pd
+# my_lambdata/assignment1.py
 
-from pd import DataFrame
+from pandas import DataFrame
 
-# State Abbreviation -> Full name and visa versa.
-# FL -> Florida, etc.
+def add_state_names(my_df):
+    """
+    Converts a dataframe with a column of state abbreviations, adding a corresponding column of state names.
+    Params:
+        my_df a pandas.DataFrame with a column called "abbrev".
+    Example:
+        add_state_names(DataFrame({"abbrev": ["CA", "CO", "CT", "DC", "TX"]}))
+    Returns: a pandas.DataFrame with the original col as well as a "name" column.
+    """
 
-df = DataFrame({'abbrev':['CA', 'CO', 'CT', 'DC', 'TX']})
-print(df.head())
+    new_frame = my_df.copy()
+
+    # need a list or dict with the abbrev/name mappings
+    names_map = {"CA": "Cali", "CO": "Colo",
+                 "CT": "Conn", "DC": "District of Columbia"}
+
+    # create a new column which maps the existing column using our names map
+    # https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.Series.map.html
+    # breakpoint()
+    # type(type(new_frame["abbrev"])) #> <class 'pandas.core.series.Series'>
+    # dir(new_frame["abbrev"])
+    new_frame["name"] = new_frame["abbrev"].map(names_map)
+
+    return new_frame
+
+
+if __name__ == "__main__":
+
+    df = DataFrame({"abbrev": ["CA", "CO", "CT", "DC", "TX"]})
+    print(df.head())
+
+    df2 = add_state_names(df)
+    print(df2.head())
